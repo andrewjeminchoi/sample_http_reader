3af42c40fbfa98263f85effb0263451ffcae2cb5
Andrew <andrewchoi96@hotmail.com>
2019-08-24 19:59:38 -0700

    Simple login and print fields on terminal

--- 00_endpoint.py
+++ 00_endpoint.py
@@ -1,5 +1,5 @@
 from bottle import route, run, static_file, debug, request, template 
-import operator
+import operator 
 
 # Serve a static homepage on load
 @route('/')
@@ -9,13 +9,32 @@ def homepage():
 
 # Function to return user keyword
 @route('/', method = 'GET')
+@route('/', method = 'POST')
 def getUsername():
-    q = request.query_string # 'GET' the query string from form
-    if q:
-        print("Q : ", q)
-    else:
-        link = "index.html"
-        return static_file(link, root='')
+    postdata = request.body.read()
+    print(postdata) 
+
+    if postdata:
+        # Try to extract form variables
+        try:
+            username = request.forms.get("username")
+            password = request.forms.get("password")
+            filename = request.forms.get("filename")
+        except:
+            link = "error.html"
+            return static_file(link, root='')
+        
+        # Execute script here
+        execute_script(username, password, filename)
+
+    link = "index.html"
+    return static_file(link, root='')
+
+def execute_script(username, password, filename):
+    print("user: ", username)
+    print("pass: ", password)
+    print("file: ", filename)
+    # Run your script here
 
 # To see page, go to localhost:8000
 run(host='localhost', port=8000, reloader=True, debug=True)
